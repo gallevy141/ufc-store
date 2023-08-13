f08919ea5779e7c870b14a04285639b00aeeb7e6
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-13 20:05:07 +0100

    adding register.js page to app/.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import MainPage from './pages/MainPage'
 import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Routes>
               <Route path="/" element={<MainPage />} />
               <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<Register />} />
               {/*other routes here as needed */}
           </Routes>
           <Footer />
