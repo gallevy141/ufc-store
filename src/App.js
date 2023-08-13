f4de9e1d92c9cd81423b41a29151b1ed4ac52f07
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-13 17:17:36 +0100

    changing mainpage import

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'font-awesome/css/font-awesome.min.css';
 //Import components
 import Header from './components/Header'
 import Footer from './components/Footer'
-import MainPage from './components/MainPage'
+import MainPage from './pages/MainPage'
 
 function App() {
     return (
