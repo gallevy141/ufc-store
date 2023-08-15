44ae6f38ee1043ed0a6810dee8398687f6c94271
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-15 09:23:40 +0100

    create cart page

---
+++ src/pages/CartPage.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Container, Row, Col, Button, Dropdown, Card } from 'react-bootstrap'
+
+function CartPage() {
+    return (
+        <Container>
+            <h2 className="mb-4">Your Shopping Cart</h2>
+
+            {/* Example cart item */}
+            <Card className="mb-3">
+                <Card.Body>
+                    <Row>
+                        <Col>Product Name</Col>
+                        <Col>
+                            <Dropdown>
+                                <Dropdown.Toggle variant="light">
+                                    Quantity: 1
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    {[1, 2, 3, 4, 5].map(qty => (
+                                        <Dropdown.Item key={qty}>
+                                            {qty}
+                                        </Dropdown.Item>
+                                    ))}
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Col>
+                        <Col>Price: $xx.xx</Col>
+                        <Col>
+                            <Button variant="danger">Delete</Button>
+                        </Col>
+                    </Row>
+                </Card.Body>
+            </Card>
+            {/* End of example cart item. */}
+
+            <Row className="align-items-center">
+                <Col>Total: $xx.xx</Col>
+                <Col className="text-right">
+                    <Button variant="primary">Checkout</Button>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default CartPage
