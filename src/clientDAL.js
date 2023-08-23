d24704d64b870fc4cb42c129b5ecd8a1d6b7aa4b
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-23 13:06:45 +0100

    adding registerUser and loginUser functions

--- src/clientDAL.js
+++ src/clientDAL.js
@@ -1,12 +1,30 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:5000/api'
+const BASE_URL = 'http://localhost:5000/api';
 
 export async function fetchProducts() {
-  try {
-    const response = await axios.get(`${BASE_URL}/products`)
-    return response.data
-  } catch (error) {
-    throw error
-  }
+    try {
+        const response = await axios.get(`${BASE_URL}/products`)
+        return response.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export async function registerUser(userData) {
+    try {
+        const response = await axios.post(`${BASE_URL}/users/register`, userData)
+        return response.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export async function loginUser(loginData) {
+    try {
+        const response = await axios.post(`${BASE_URL}/users/login`, loginData)
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
