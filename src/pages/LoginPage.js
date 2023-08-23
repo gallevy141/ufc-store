082a1598172e6b82321276cc4687f09b7edb8c4b
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-23 13:42:51 +0100

    adding useState

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
