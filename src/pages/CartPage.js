1c6ff0a2f8fc1750fe441998ededb4d4c59136b1
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-09-14 10:52:08 +0100

    adding logic to retrieve usersID and fetch cart items

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,44 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, Row, Col, Button, Dropdown, Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { getLoggedInUser, fetchCartItems } from '../clientDAL'
 
 function CartPage() {
+    const [userId, setUserId] = useState(null)
+    const [cartItems, setCartItems] = useState([])
+
+    useEffect(() => {
+
+        async function fetchUser() {
+            try {
+                const userData = await getLoggedInUser()
+                setUserId(userData.userId)
+
+                const cartData = await fetchCartItems(userData.userId)
+                setCartItems(cartData)
+            } catch (error) {
+                console.error("Error fetching user or cart data:", error)
+            }
+        }
+        fetchUser()
+    }, [])
+
     return (
         <Container>
             <h2 className="mb-4">Your Shopping Cart</h2>
-
-            {/* Example cart item */}
-            <Card className="mb-3">
-                <Card.Body>
-                    <Row>
-                        <Col>Product Name</Col>
-                        <Col>
-                            <Dropdown>
-                                <Dropdown.Toggle variant="light">
-                                    Quantity: 1
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    {[1, 2, 3, 4, 5].map(qty => (
-                                        <Dropdown.Item key={qty}>
-                                            {qty}
-                                        </Dropdown.Item>
-                                    ))}
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </Col>
-                        <Col>Price: $xx.xx</Col>
-                        <Col>
-                            <Button variant="danger">Delete</Button>
-                        </Col>
-                    </Row>
-                </Card.Body>
-            </Card>
-            {/* End of example cart item. */}
+            {cartItems.map(item => (
+                <Card key={item.productId} className="mb-3"> 
+                    {/* ... Display cart item details ... */}
+                </Card>
+            ))}
 
             <Row className="align-items-center">
-                <Col>Total: $xx.xx</Col>
+                <Col>Total: $xx.xx</Col>  {/* Update this with actual total */}
                 <Col className="text-right">
-                <Button as={Link} to="/checkout" variant="primary">Checkout</Button>
+                    <Button as={Link} to="/checkout" variant="primary">Checkout</Button>
                 </Col>
             </Row>
         </Container>
