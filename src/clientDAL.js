1e7a62224a8e9fea8a13cd6c8d16f95dcfb7939d
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-09-14 10:39:01 +0100

    adding cart api calls

--- src/clientDAL.js
+++ src/clientDAL.js
@@ -38,3 +38,42 @@ export async function loginUser(loginData) {
         throw error
     }
 }
+
+export const fetchCartItems = async (userId) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/cart/${userId}`)
+        if (response.status === 200) {
+            return response.data
+        } else {
+            throw new Error('Failed to fetch cart items')
+        }
+    } catch (error) {
+        throw error
+    }
+}
+
+export const addProductToCart = async (userId, productId) => {
+    try {
+        const response = await axios.post(`${BASE_URL}/cart/${userId}/add`, { productId, quantity: 1 })
+        if (response.status === 200) {
+            return response.data
+        } else {
+            throw new Error('Failed to add product to cart')
+        }
+    } catch (error) {
+        throw error
+    }
+}
+
+export const removeProductFromCart = async (userId, productId) => {
+    try {
+        const response = await axios.delete(`${BASE_URL}/cart/${userId}/remove/${productId}`)
+        if (response.status === 200) {
+            return response.data
+        } else {
+            throw new Error('Failed to remove product from cart')
+        }
+    } catch (error) {
+        throw error
+    }
+}
