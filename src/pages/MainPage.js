7f7d4cb18b76f3216c4cf2d13b6eb9dc21f8d3cf
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-09-19 19:33:10 +0100

    adding functionality that pressing on a product directs user to specific product page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Container, Row, Col, Card, Button } from 'react-bootstrap'
-import { fetchProducts } from '../clientDAL' 
+import { Link } from 'react-router-dom'
+import { fetchProducts } from '../clientDAL'
 
 const MainPage = () => {
     const [products, setProducts] = useState([])
@@ -34,13 +35,15 @@ const MainPage = () => {
             <Row className="my-4">
                 {products.map(product => (
                     <Col md={4} key={product.productID}>
-                        <Card>
-                            <Card.Img variant="top" src={product.image} />
-                            <Card.Body>
-                                <Card.Title>{product.name}</Card.Title>
-                                <Button variant="primary">Add to Cart</Button>
-                            </Card.Body>
-                        </Card>
+                        <Link to={`/product/${product.productID}`} className="text-decoration-none text-dark">
+                            <Card>
+                                <Card.Img variant="top" src={product.image} />
+                                <Card.Body>
+                                    <Card.Title>{product.name}</Card.Title>
+                                    <Button variant="primary">Add to Cart</Button>
+                                </Card.Body>
+                            </Card>
+                        </Link>
                     </Col>
                 ))}
             </Row>
