d42f1f313a7385faa9b7498f8dfc24a3194dcb7d
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-09-19 18:08:16 +0100

    updating footer and adding link pathways to about, contact and t&c

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
         <Container fluid style={{ backgroundColor: '#101010', color: 'white', fontFamily: 'Oswald, sans-serif' }} className="py-3">
             <Row>
                 <Col md={4} className="d-flex justify-content-center justify-content-md-start">
-                    <div className="pr-3">About Us</div>
-                    <div className="pr-3">|</div>
-                    <div className="pr-3">Contact Us</div>
-                    <div>|</div>
-                    <div className="pl-3">T&C</div>
+                    <Link to="/about" className="text-white pr-3">About Us</Link>
+                    <Link to="/contact" className="text-white pr-3">Contact Us</Link>
+                    <Link to="/termsandconditions" className="text-white">Terms & Conditions</Link>
                 </Col>
                 <Col md={4} className="text-center">
                     <a href="https://twitter.com/ufc?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank" rel="noopener noreferrer" className="pr-3 text-white">Twitter</a>
-                    <div>|</div>
                     <a href="https://www.instagram.com/ufc/?hl=en" target="_blank" rel="noopener noreferrer" className="pl-3 text-white">Instagram</a>
                 </Col>
                 <Col md={4} className="d-flex justify-content-center justify-content-md-end">
